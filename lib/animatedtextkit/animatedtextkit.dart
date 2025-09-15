2d67c79a04497880844343462d1f58da20e24028
Muhammad Ehsan <mehsan4270@gmail.com>
2025-09-15 12:29:18 +0500

    upload widget

---
+++ lib/animatedtextkit/animatedtextkit.dart
@@ -0,0 +1,53 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flutter/material.dart';
+
+class AnimatedTextKitScreen extends StatelessWidget {
+  const AnimatedTextKitScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Animated Text Kit"),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+      ),
+      body: AnimatedTextKit(
+        animatedTexts: [
+          FadeAnimatedText(
+            "Welcome to the flutter development course",
+            textStyle: TextStyle(
+              color: Colors.red,
+              fontSize: 20,
+              fontWeight: FontWeight.w600,
+            ),
+            duration: Duration(seconds: 2),
+          ),
+          ScaleAnimatedText(
+            "Im flutter developer also",
+            textStyle: TextStyle(
+              color: Colors.red,
+              fontSize: 20,
+              fontWeight: FontWeight.w600,
+            ),
+            duration: Duration(seconds: 2),
+          ),
+          RotateAnimatedText(
+            "Thanks for the course",
+            textStyle: TextStyle(
+              color: Colors.red,
+              fontSize: 20,
+              fontWeight: FontWeight.w600,
+            ),
+            duration: Duration(seconds: 2),
+          ),
+
+        ],
+        pause: Duration(milliseconds: 600),
+        stopPauseOnTap: true,
+        totalRepeatCount: 100,
+        displayFullTextOnTap: true,
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:packages_app/scratcher/scratcher.dart';
 import 'package:packages_app/steppertouch/steppertouch.dart';
 
+import 'animatedtextkit/animatedtextkit.dart';
 import 'floatingdraggablewidget/floatingdraggablewidget.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: FloatingDraggableWidgetScreen(),
+    home: AnimatedTextKitScreen(),
   ));
 }
